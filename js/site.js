0f05c3bc73b313a5a9c59d08a2139594b28f8732
Mohammed Wasfi <mohammedwasfi@dhcp79.merunorth104.iit.edu>
2018-04-12 11:59:02 -0500

    Remove navigation bad and slideshow

--- js/site.js
+++ js/site.js
@@ -7,38 +7,6 @@
 $('html').removeClass('no-js').addClass('js');
 $.noConflict();
 
-(function($) {
-  "use strict";
-  var slideshow = (function () {
-    var counter = 0,
-      i,
-      j,
-      slides =  $("#slideshow .slide"),
-      slidesLen = slides.length - 1;
-    for (i = 0, j = 9999; i < slides.length; i += 1, j -= 1) {
-      $(slides[i]).css("z-index", j);
-    }
-    return {
-      startSlideshow: function () {
-        window.setInterval(function () {
-          if (counter === 0) {
-            slides.eq(counter).fadeOut();
-            counter += 1;
-          } else if (counter === slidesLen) {
-            counter = 0;
-            slides.eq(counter).fadeIn(function () {
-              slides.fadeIn();
-            });
-          } else {
-            slides.eq(counter).fadeOut();
-            counter += 1;
-          }
-        }, 2500);
-      }
-    };
-  }());
-  slideshow.startSlideshow();
-
   $('#inputs li').on('click', function() {
     $(this).find('input').focus();
   });
